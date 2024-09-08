b9e01c621e4b9b72cdf9047ce4ccda07e8833ebd
cauaqroz <cauaqroz@gmail.com>
2024-09-08 12:15:00 -0300

    deploy

--- src/main/java/br/com/cauaqroz/ConectaPlus/config/WebConfig.java
+++ src/main/java/br/com/cauaqroz/ConectaPlus/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://frontend-conecta-f069212eea10.herokuapp.com") // URL do seu frontend
+                        .allowedOrigins("https://frontend-conecta-f069212eea10.herokuapp.com", "http://localhost:3000/") // URL do seu frontend
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                         .allowedHeaders("*")
                         .allowCredentials(true);
