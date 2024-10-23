3190918d4fb7592e6d8a5e033684b63b27072ce0
cauaqroz <cauaqroz@gmail.com>
2024-10-23 10:20:11 -0300

    deploy

---
+++ src/main/java/br/com/cauaqroz/ConectaPlus/config/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package br.com.cauaqroz.ConectaPlus.config;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleException(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("{\"error\": \"" + e.getMessage() + "\"}");
+    }
+}
