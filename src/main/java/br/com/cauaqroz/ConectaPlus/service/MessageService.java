46e6cd212bb30fc75a7d52b369f72990378b6bf0
cauaqroz <cauaqroz@gmail.com>
2024-10-23 10:31:35 -0300

    deploy

--- src/main/java/br/com/cauaqroz/ConectaPlus/service/MessageService.java
+++ src/main/java/br/com/cauaqroz/ConectaPlus/service/MessageService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.logging.Logger;
 
 @Service
 public class MessageService implements IMessageService {
@@ -25,19 +26,27 @@ public class MessageService implements IMessageService {
     @Autowired
     private UserRepository userRepository;
 
+    private static final Logger logger = Logger.getLogger(MessageService.class.getName());
+
     @Override
     public Message sendMessage(String channelId, Message message) throws Exception {
+        logger.info("Iniciando envio de mensagem...");
         String senderId = message.getSender();
         Optional<User> user = userRepository.findById(senderId);
         if (user.isPresent()) {
             message.setSenderName(user.get().getName());
         } else {
+            logger.severe("Usuário não encontrado: " + senderId);
             throw new Exception("Usuário não encontrado");
         }
 
-        Channel channel = channelRepository.findById(channelId).orElseThrow(() -> new Exception("Canal não encontrado"));
+        Channel channel = channelRepository.findById(channelId).orElseThrow(() -> {
+            logger.severe("Canal não encontrado: " + channelId);
+            return new Exception("Canal não encontrado");
+        });
 
         if (!channel.getMasterUserId().equals(senderId) && !channel.getAllowedUserIds().contains(senderId)) {
+            logger.severe("Usuário não tem permissão para enviar mensagens neste canal: " + senderId);
             throw new Exception("Usuário não tem permissão para enviar mensagens neste canal.");
         }
 
@@ -50,6 +59,7 @@ public class MessageService implements IMessageService {
         channel.getMessages().add(message);
         channelRepository.save(channel);
 
+        logger.info("Mensagem enviada com sucesso.");
         return message;
     }
 
